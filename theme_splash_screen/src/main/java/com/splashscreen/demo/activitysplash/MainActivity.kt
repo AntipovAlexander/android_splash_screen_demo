b5863682ed9a81101bfac1ba24053eb34de730d4
Alexander Antipov <antipov@themindstudios.com>
2021-09-27 17:01:47 +0300

    added the third app

---
+++ theme_splash_screen/src/main/java/com/splashscreen/demo/activitysplash/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.splashscreen.demo.activitysplash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.splashscreen.demo.splashscreenempty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
