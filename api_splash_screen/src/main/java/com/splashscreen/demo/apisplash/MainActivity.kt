aa4255635f9deb4351406647a6288573cd0e196f
Alexander Antipov <antipov@themindstudios.com>
2021-09-27 17:41:34 +0300

    added app for api splash

---
+++ api_splash_screen/src/main/java/com/splashscreen/demo/apisplash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.splashscreen.demo.apisplash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.splashscreen.demo.splashscreenempty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
