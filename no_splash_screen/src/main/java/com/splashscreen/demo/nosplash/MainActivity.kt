549267e99bb4f357aa74a5fc827520dfd62b86ea
Alexander Antipov <antipov@themindstudios.com>
2021-09-27 16:35:56 +0300

    renamed package in the 1st app

--- no_splash_screen/src/main/java/com/splashscreen/demo/nosplash/MainActivity.kt
+++ no_splash_screen/src/main/java/com/splashscreen/demo/nosplash/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.splashscreen.demo.splashscreenempty
+package com.splashscreen.demo.nosplash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.splashscreen.demo.splashscreenempty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
