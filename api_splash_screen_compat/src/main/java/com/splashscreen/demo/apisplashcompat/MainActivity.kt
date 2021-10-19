46260c61d9f7fad55f44955b48358fe5b6ca946e
Alexander Antipov <antipov@themindstudios.com>
2021-10-19 16:40:35 +0300

    added compat animations

--- api_splash_screen_compat/src/main/java/com/splashscreen/demo/apisplashcompat/MainActivity.kt
+++ api_splash_screen_compat/src/main/java/com/splashscreen/demo/apisplashcompat/MainActivity.kt
@@ -2,73 +2,67 @@ package com.splashscreen.demo.apisplashcompat
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.get
 import com.splashscreen.demo.splashscreenempty.R
 
 class MainActivity : AppCompatActivity() {
 
-    private var dataIsLoaded = false
+    private var keepOnScreen = true
     private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        val splash = installSplashScreen()
+        splash.setKeepVisibleCondition { keepOnScreen }
         setContentView(R.layout.activity_main)
-        keepPlaceholderOnScreen()
         fakeWork()
-        animateSplashScreenCompat()
-    }
-
-    private fun keepPlaceholderOnScreen() {
-        val content: View = findViewById(android.R.id.content)
-        content.viewTreeObserver.addOnPreDrawListener {
-            return@addOnPreDrawListener dataIsLoaded
-        }
+        animateSplashScreenCompat(splash)
     }
 
     private fun fakeWork() {
-        handler.postDelayed({
-            dataIsLoaded = true
-        }, 2000)
+        handler.postDelayed({ keepOnScreen = false }, 2000)
     }
 
-    private fun animateSplashScreenCompat() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val animationDuration = 1000L
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                val animatorSet = AnimatorSet()
-                val slideUp = ObjectAnimator.ofFloat(
-                    splashScreenView.iconView,
-                    View.Y,
-                    0f
-                )
-                val alpha = ObjectAnimator.ofFloat(
-                    splashScreenView,
-                    View.ALPHA,
-                    1f,
-                    0f
-                )
+    private fun animateSplashScreenCompat(splash: SplashScreen) {
+        val animationDuration = 1000L
+        splash.setOnExitAnimationListener { splashScreenView ->
+            val animatorSet = AnimatorSet()
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView.iconView,
+                View.Y,
+                0f
+            )
+            animatorSet.play(slideUp)
+            val alpha = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.ALPHA,
+                1f,
+                0f
+            )
+            animatorSet.play(alpha)
+            (splashScreenView.view as ViewGroup).getChildAt(1)?.let {
                 val slideDown = ObjectAnimator.ofFloat(
-                    splashScreenView[1],
+                    (splashScreenView.view as ViewGroup)[1],
                     View.Y,
-                    splashScreenView.measuredHeight.toFloat()
+                    splashScreenView.view.measuredHeight.toFloat()
                 )
-                animatorSet.apply {
-                    playTogether(slideUp, alpha, slideDown)
-                    interpolator = LinearInterpolator()
-                    duration = animationDuration
-                    doOnEnd { splashScreenView.remove() }
-                    start()
-                }
+                animatorSet.play(slideDown)
+            }
+            animatorSet.apply {
+                interpolator = LinearInterpolator()
+                duration = animationDuration
+                doOnEnd { splashScreenView.remove() }
+                start()
             }
         }
     }
--- theme_splash_screen/src/main/java/com/splashscreen/demo/themesplash/MainActivity.kt
+++ theme_splash_screen/src/main/java/com/splashscreen/demo/themesplash/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.splashscreen.demo.themesplash
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.view.get
 import com.splashscreen.demo.splashscreenempty.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +16,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_App)
         setContentView(R.layout.activity_main)
+        animateSplashScreenCompat()
+    }
+
+    private fun animateSplashScreenCompat() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val animationDuration = 1000L
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                val animatorSet = AnimatorSet()
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView.iconView,
+                    View.Y,
+                    0f
+                )
+                val alpha = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.ALPHA,
+                    1f,
+                    0f
+                )
+                val slideDown = ObjectAnimator.ofFloat(
+                    splashScreenView[1],
+                    View.Y,
+                    splashScreenView.measuredHeight.toFloat()
+                )
+                animatorSet.apply {
+                    playTogether(slideUp, alpha, slideDown)
+                    interpolator = LinearInterpolator()
+                    duration = animationDuration
+                    doOnEnd { splashScreenView.remove() }
+                    start()
+                }
+            }
+        }
     }
 }
